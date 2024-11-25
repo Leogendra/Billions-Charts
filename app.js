594ab329839b3f57df0951bee3526a43f024fb20
Gatien-IV <le.magicien.annonyme@gmail.com>
2024-11-25 21:11:01 +0100

    POC v0.1

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-const { get_token, get_full_token, get_user_id } = require("./backend/spotify");
+const { get_token } = require("./backend/spotify");
 const bodyParser = require("body-parser");
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
-const app = express();
-const PORT = process.env.PORT || 5500;
-const CLIENT_ID = process.env.CLIENT_ID;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-
 const VERBOSE = true;
+const PORT = process.env.PORT || 3500;
 
+const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "./public")));
+
+
+
+
+app.post("/dev", async (_, res) => {
+    try {
+        const token = await get_token();
+        if (token) {
+            res.json({ message: "Token Got" });
+        }
+        else {
+            res.status(500).json({ message: "Unknown error." });
+        }
+    }
+    catch (error) {
+        console.error("Error retrieving genre:", error);
+        res.status(500).json({ message: "Error retrieving genre." });
+    }
+});
 
 
 
