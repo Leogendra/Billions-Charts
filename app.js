7f9aa92ef3254fb38ac8b1f74db6509bff8bfa44
Gatien-IV <le.magicien.annonyme@gmail.com>
2024-12-14 17:14:38 +0100

    Proper tracks fetching

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ require("dotenv").config();
 
 const VERBOSE = true;
 const PORT = process.env.PORT || 3500;
-const PLAYLIST_URL = process.env.PLAYLIST_URL;
+const PLAYLIST_ID = process.env.PLAYLIST_ID;
+const reportFile = "data/report.json";
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,25 +18,86 @@ app.use(express.static(path.join(__dirname, "./public")));
 
 
 
-app.post("/dev", async (_, res) => {
-    
-    const tracksFile = "data/tracks.json";
-    const outputFile = "data/report.json";
-
+app.post("/get-report", async (_, res) => {
+    // get the most recent tracks file
+    const tracksFile = fs.readdirSync("data").filter(file => file.startsWith("tracks_")).sort().reverse()[0];
     try {
-        const token = true
-        if (token) {
-            await generate_report(tracksFile, outputFile);
-            res.json({ message: "Token Got" });
+        if (fs.existsSync(reportFile)) {
+            const report = JSON.parse(fs.readFileSync(reportFile, "utf-8"));
+            res.json(report);
         }
         else {
-            res.status(500).json({ message: "Unknown error." });
+            await generate_report(tracksFile, reportFile);
+            const report = JSON.parse(fs.readFileSync(reportFile, "utf-8"));
+            res.json(report);
         }
     }
     catch (error) {
-        console.error("Error retrieving genre:", error);
-        res.status(500).json({ message: "Error retrieving genre." });
+        console.error("Error while retrieving report:", error);
+        res.status(500).json({ message: "Error retrieving report." });
     }
+});
+
+
+app.post("/retrieve-tracks", async (req, res) => {
+    const { password } = req.body;
+
+    // date with format YYYY-MM-DD
+    const tracksFile = `data/tracks_${new Date().toISOString().split("T")[0]}.json`;
+    console.log("Tracks file:", tracksFile);
+
+    if (password === process.env.PASSWORD) {
+        // Fetch tracks
+        try {
+            console.log("Fetching tracks...");
+            await fetch_playlist(PLAYLIST_ID, tracksFile);
+        }
+        catch (error) {
+            console.error("Error while retrieving tracks:", error);
+            res.status(500).json({ message: "Error retrieving tracks." });
+        }
+
+        // Then get playcounts
+        try {
+            console.log("Getting playcounts...");
+            await get_playcount(tracksFile);
+        }
+        catch (error) {
+            console.error("Error while retrieving playcounts:", error);
+            res.status(500).json({ message: "Error retrieving playcounts." });
+        }
+
+        // Then clean playlist
+        try {
+            console.log("Cleaning playlist...");
+            await clean_playlist(tracksFile);
+        }
+        catch (error) {
+            console.error("Error while cleaning playlist:", error);
+            res.status(500).json({ message: "Error cleaning playlist." });
+        }
+
+        // Then generate report
+        try {
+            console.log("Generating report...");
+            await generate_report(tracksFile, reportFile);
+        }
+        catch (error) {
+            console.error("Error while generating report:", error);
+            res.status(500).json({ message: "Error generating report." });
+        }
+
+        res.json({ message: "Tracks retrieved and report generated." });
+    }
+    else {
+        console.log("# Invalid password attempt.");
+        res.status(401).json({ message: "Invalid password." });
+    }
+});
+
+
+app.post("/dev", async (_, res) => {
+    res.json({ message: "Ping!" });
 });
 
 
