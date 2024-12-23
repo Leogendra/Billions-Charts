1ab155450d8cfc97b4624932c079bdd44f0d43ae
Gatien-IV <le.magicien.annonyme@gmail.com>
2024-12-24 00:30:45 +0100

    Playcount error

--- scrapper.py
+++ scrapper.py
@@ -80,7 +80,7 @@ def fetch_tracks(outputFile):
         print(f"Tracks saved in {outputFile}")
 
 
-def clean_playlist(tracksPath):
+def clean_playlist(tracksPath, cleanedPath):
     with open(tracksPath, 'r', encoding='utf-8') as file:
         playlist = json.load(file)
 
@@ -109,10 +109,10 @@ def clean_playlist(tracksPath):
     ]
 
     playlist["items"] = cleaned_tracks
-    with open(tracksPath, 'w', encoding='utf-8') as file:
+    with open(cleanedPath, 'w', encoding='utf-8') as file:
         json.dump(playlist, file, indent=4)
 
-    print(f"Playlist cleaned and saved in {tracksPath}")
+    print(f"Playlist cleaned and saved in {cleanedPath}")
 
 
 def get_play_count_with_selenium(track_id):
@@ -123,7 +123,7 @@ def get_play_count_with_selenium(track_id):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    service = Service("/usr/bin/chromedriver")
+    service = Service()
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     try:
@@ -149,25 +149,32 @@ def process_track(track):
     return track
 
 
-def retrieve_playcounts(full_data, outputPath):
-    tracks = full_data["data"]["items"]
+def retrieve_playcounts(tracksPath):
+    if os.path.exists(tracksPath):
+        with open(tracksPath, "r", encoding="utf-8") as f:
+            tracks_data = json.load(f)
+    else:
+        raise FileNotFoundError("The file does not exist.")
+    
+    tracks = tracks_data["items"]
+
     for i in range(len(tracks)):
         track = tracks[i]
-        print(f"Traitement de la piste {i+1}/{len(tracks)}...", end="\r")
-        if ("playcount" not in track) or (track["playcount"] is None):
+        print(f"Processing track {i+1}/{len(tracks)}...", end="\r")
+        if ("playcount" not in track) or (track["playcount"] is None) or (track["playcount"] <= 0):
             track_id = track["id"]
-            play_count = get_play_count_with_selenium(track_id)
-            if (play_count):
-                tracks[i]["playcount"] = play_count
+            playcount = get_play_count_with_selenium(track_id)
+            if (playcount):
+                tracks[i]["playcount"] = playcount
             else:
-                print(f"Playcount manquant pour {track['name']}")
+                print(f"Playcount not found for {track['name']}")
 
-    full_data["data"]["items"] = tracks
+    tracks_data["items"] = tracks
 
-    with open(outputPath, "w", encoding="utf-8") as f:
-        json.dump(full_data, f, ensure_ascii=False, indent=4)
+    with open(tracksPath, "w", encoding="utf-8") as f:
+        json.dump(tracks_data, f, ensure_ascii=False, indent=4)
 
-    print("Mise à jour du fichier test.json terminée.")
+    print(f"Playcounts retrieved and saved in {tracksPath}")
 
 
 def format_number(number):
@@ -177,63 +184,10 @@ def format_number(number):
 
 if __name__ == "__main__":
 
-    # format YYYY-MM-DD
     TIME_KEY = datetime.datetime.now().strftime("%Y-%m-%d")
     dataPath = f"data/tracks_{TIME_KEY}.json"
+    dataCleanedPath = f"data/tracks_{TIME_KEY}_cleaned.json"
 
     fetch_tracks(dataPath)
-    clean_playlist(dataPath)
-    exit(0)
-
-
-
-
-    if os.path.exists(dataPath):
-        with open(dataPath, "r", encoding="utf-8") as f:
-            tracks_data = json.load(f)
-    else:
-        raise FileNotFoundError("Le fichier billions.json est introuvable.")
-
-    retrieve_playcounts(tracks_data, dataPath)
-
-    tracks = tracks_data["data"]["items"]
-    print(f"Nombre de pistes à traiter: {len(tracks)}")
-
-    tracks.sort(key=lambda x: x["playcount"], reverse=True)
-
-    print("Top des pistes les plus écoutées:")
-    with open("data/leaderboard_tracks.txt", "w", encoding="utf-8") as f:
-        for i in range(len(tracks)):
-            track = tracks[i]
-            f.write(f"{i+1}. {format_number(track['playcount'])} - {track['name'].split("-")[0].split("(")[0].strip()}, by {', '.join(track['artists'][i]['name'] for i in range(len(track['artists'])))}\n")
-
-    artists_count = {}
-    for track in tracks:
-        for artist in track["artists"]:
-            artist_name = artist["name"]
-            artists_count[artist_name] = artists_count.get(artist_name, 0) + 1
-    
-    artists_count = [{"name": k, "count": v} for k, v in artists_count.items()]
-    artists_count.sort(key=lambda x: x["count"], reverse=True)
-
-    
-    print("Top des artistes avec les plus de sons à 1B+:")
-    with open("data/leaderboard_artists.txt", "w", encoding="utf-8") as f:
-        for i in range(len(artists_count)):
-            artist = artists_count[i]
-            f.write(f"{i+1}. {artist['name']}: {artist['count']} tracks \n")
-    
-    artists_playcount = {}
-    for track in tracks:
-        for artist in track["artists"]:
-            artist_name = artist["name"]
-            artists_playcount[artist_name] = artists_playcount.get(artist_name, 0) + track["playcount"]
-    
-    artists_playcount = [{"name": k, "playcount": v} for k, v in artists_playcount.items()]
-    artists_playcount.sort(key=lambda x: x["playcount"], reverse=True)
-
-    print("Top des artistes avec les plus de streams:")
-    with open("data/leaderboard_streams.txt", "w", encoding="utf-8") as f:
-        for i in range(len(artists_playcount)):
-            artist = artists_playcount[i]
-            f.write(f"{i+1}. {artist['name']}: {format_number(artist['playcount'])} tracks \n")
+    clean_playlist(dataPath, dataCleanedPath)
+    retrieve_playcounts(dataCleanedPath)
