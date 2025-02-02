8a611c0fb0047a412b75b80cbcca1d26f49265e6
Gatien-IV <le.magicien.annonyme@gmail.com>
2025-02-02 17:17:11 +0100

    header

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ import json
 
 
 # query an artists
-artist_url = "0wRGuF39WWxHeftv2TBNQT"
+artist_url = "672zgt6YECMA7Om9h5V2HE"
 artist = Artist()
 artists_infos = artist.get_artist(artist_url)
 
@@ -14,5 +14,5 @@ if (artists_infos["data"] is not None):
 else:
     print("!!! Test failed !!!")
 
-with open("test.json", "w", encoding="utf-8") as f:
+with open("tests/test.json", "w", encoding="utf-8") as f:
     f.write(json.dumps(artists_infos, indent=4))
