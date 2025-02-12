c401466100907ce75a641634cbf2ca3b7338b2d5
Leogendra <le.magicien.annonyme@gmail.com>
2025-02-12 12:53:17 +0100

    Add Artists bars

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -64,43 +64,89 @@ async function toggle_all_details() {
 }
 
 
-async function display_artists() {
-    const artists = [
-        { name: "The Weeknd", streams: 12000_000, image: "https://i.scdn.co/image/ab6761610000e5eb9e528993a2820267b97f6aae" },
-        { name: "Taylor Swift", streams: 100_000, image: "https://i.scdn.co/image/ab6761610000e5eb9e528993a2820267b97f6aae" },
-        { name: "Drake", streams: 80000, image: "https://i.scdn.co/image/ab6761610000e5eb9e528993a2820267b97f6aae" },
-        { name: "Ariana Grande", streams: 100, image: "https://i.scdn.co/image/ab6761610000e5eb9e528993a2820267b97f6aae" },
-    ];
+function display_artists_bars(artists_infos, containerId) {
+    const maxStreams = Math.max(...artists_infos.map(artists_infos => artists_infos.data));
+    const container = document.getElementById(containerId);
 
-    const maxStreams = Math.max(...artists.map(artist => artist.streams)); // Trouve le plus grand nombre de streams
-    const container = document.getElementById("bar-container");
+    const minWidth = 250; // Min width in pixels
+    const maxWidth = 100; // Max width in percentage
 
-    // Largeurs min et max
-    const minWidth = 120; // Largeur minimale pour les petites valeurs
-    const maxWidth = 100; // Largeur max en pourcentage du conteneur
-
-    // Génération dynamique des barres
-    artists.forEach(artist => {
+    artists_infos.forEach(artist => {
         const bar = document.createElement("div");
-        bar.classList.add("bar");
+        bar.classList.add("bar-artist");
 
-        // Calcule la largeur proportionnelle avec un minimum
-        let calculatedWidth = (artist.streams / maxStreams) * maxWidth;
-        let finalWidth = Math.max(calculatedWidth, (minWidth / container.offsetWidth) * 100); // Min 120px converti en %
+        let calculatedWidth = (artist.data / maxStreams) * maxWidth;
+        let finalWidth = Math.max(calculatedWidth, (minWidth / container.offsetWidth) * 100); // Convert minWidth to percentage
 
         bar.style.width = `${finalWidth}%`;
 
         const text = document.createElement("span");
-        text.textContent = artist.name;
+        text.innerHTML = `<a class="cta-link" href="${artist.url}" target="_blank">${artist.name}</a> (${artist.data_dislay})`;
         bar.appendChild(text);
 
         const img = document.createElement("img");
         img.src = artist.image;
-        img.classList.add("artist-image");
+        img.classList.add("bar-artist-image");
         bar.appendChild(img);
 
         container.appendChild(bar);
     });
+}
+
+
+async function update_artists_most_streamed(report) {
+    const artists = [];
+    report.artists_playcounts.forEach(artist => {
+        artists.push({
+            name: artist[0],
+            data: artist[1],
+            data_dislay: `${(artist[1]/1_000_000_000).toFixed(2)}B`,
+            url: `https://open.spotify.com/artist/${artist[2]}`,
+            image: artist[3]
+        });
+    });
+
+    // Fake data
+    // artists.push({
+    //     name: "Ariana Grande (fake)",
+    //     data: 1_000_000_000,
+    //     url: `https://open.spotify.com/`,
+    //     image: "https://i.scdn.co/image/ab6761610000e5eb9e528993a2820267b97f6aae",
+    // });
+
+    display_artists_bars(artists, "artists-most-streamed");
+}
+
+
+async function update_artists_most_songs(report) {
+    const artists = [];
+    report.artists_counts.forEach(artist => {
+        artists.push({
+            name: artist[0],
+            data: artist[1],
+            data_dislay: artist[1],
+            url: `https://open.spotify.com/artist/${artist[2]}`,
+            image: artist[3]
+        });
+    });
+
+    display_artists_bars(artists, "artists-most-songs");
+}
+
+
+async function update_artists_most_time(report) {
+    const artists = [];
+    report.artists_length.forEach(artist => {
+        artists.push({
+            name: artist[0],
+            data: artist[1],
+            data_dislay: `${String(Math.floor(artist[1] / 60000)).padStart(2, '0')}:${String(Math.floor((artist[1] % 60000) / 1000)).padStart(2, '0')}`,
+            url: `https://open.spotify.com/artist/${artist[2]}`,
+            image: artist[3]
+        });
+    });
+
+    display_artists_bars(artists, "artists-most-time");
 }
 
 
@@ -113,6 +159,7 @@ async function main() {
     await Promise.all([
         update_counters(report),
         update_playlist_infos(report),
+
         update_new_entries(report),
         update_trendings(report),
         update_newest(report),
@@ -121,13 +168,17 @@ async function main() {
         update_least_streamed(report),
         update_most_long(report),
         update_most_short(report),
+
+        update_artists_most_streamed(report),
+        update_artists_most_songs(report),
+        update_artists_most_time(report),
+
         place_arrow()
     ]);
 
     // await all
     add_scrolling_cards();
 
-    // display_artists();
 }
 
 
