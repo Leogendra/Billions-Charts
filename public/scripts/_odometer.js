fd88c5aad473520bc246bc5f9bbfbababd9e2f34
Gatien-IV <le.magicien.annonyme@gmail.com>
2025-02-24 21:00:27 +0100

    Feat(odometer): add suffix for odometer

--- public/scripts/_odometer.js
+++ public/scripts/_odometer.js
@@ -1,3 +1,5 @@
+// This is a copy of the original odometer.js file with some modifications to allow the use of a suffix after the numbers.
+
 (function () {
     var COUNT_FRAMERATE, COUNT_MS_PER_FRAME, DIGIT_FORMAT, DIGIT_HTML, DIGIT_SPEEDBOOST, DURATION, FORMAT_MARK_HTML, FORMAT_PARSER, FRAMERATE, FRAMES_PER_VALUE, MS_PER_FRAME, MutationObserver, Odometer, RIBBON_HTML, TRANSITION_END_EVENTS, TRANSITION_SUPPORT, VALUE_HTML, addClass, createFromHTML, fractionalPart, now, removeClass, requestAnimationFrame, round, transitionCheckStyles, trigger, truncate, wrapJQuery, _jQueryWrapped, _old, _ref, _ref1,
         __slice = [].slice;
